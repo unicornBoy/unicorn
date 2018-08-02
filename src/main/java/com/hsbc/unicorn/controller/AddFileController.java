02dd70e8f1a2d94699d0182ab4b0848917034b2a
ToDongMing <35171431+ToDongMing@users.noreply.github.com>
2018-08-02 15:44:49 +0800

    新增插入数据部分代码

--- src/main/java/com/hsbc/unicorn/controller/AddFileController.java
+++ src/main/java/com/hsbc/unicorn/controller/AddFileController.java
@@ -1,1 +1,24 @@
-sss
+package com.hsbc.unicorn.controller;
+
+import com.hsbc.unicorn.entity.CloudFiles;
+import com.hsbc.unicorn.repository.CloudFilesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author:lmm
+ * @date:2018/8/2 
+ * @description:
+ */
+@RestController()
+@RequestMapping("/cloudfiles")
+public class AddFileController {
+    @Autowired
+    private CloudFilesRepository cloudFilesRepository;
+
+    @PostMapping("/add")
+    public void save(@RequestBody CloudFiles cloudFiles) {
+        cloudFilesRepository.save(cloudFiles);
+    }
+}
+
